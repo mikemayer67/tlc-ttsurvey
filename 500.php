d46ea656d20e42d7b196050406a8f7f3efbbe714
Michael A Mayer <mikemayer67@vmwishes.com>
2025-03-11 01:31:44 -0400

    try again

--- 500.php
+++ 500.php
@@ -6,8 +6,8 @@ require_once(app_file('include/const.php'));
 require_once(app_file('include/logger.php'));
 require_once(app_file('common/page_elements.php'));
 
-$dir = dir_name($_SERVER['SCRIPT_NAME']);
-$img = preg_replace('#//#','/', "$url/img/405.png");
+$url = dirname($_SERVER['SCRIPT_NAME']);
+$img = preg_replace('#//#','/', "$url/img/500.png");
 
 $contact = ADMIN_CONTACT;
 $pronoun = ADMIN_PRONOUN;
@@ -20,7 +20,7 @@ navbar();
 ?>
 
 <div style='width:80%; max-width:600px; margin-top:5%; margin-left:auto; margin-right:auto;'>
-  <a href='<?=$dir?>'>
+  <a href='<?=$url?>'>
     <img src='<?=$img?>' alt='Something went terribly wrong' style='width:100%;'>
   </a>
 </div>
