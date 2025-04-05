afc932256347a96412867eedb81a1c05b707066d
Michael A Mayer <mikemayer67@vmwishes.com>
2025-04-04 22:27:14 -0400

    caching of tab data

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,29 @@
 
-$(document).ready(
-  function($) {
-    $('#ttt-body').show();
-    console.log("Hello from admin.js");
+var cache_cb = null;
+
+
+( function() {
+
+  var ce = {};
+
+  function handle_tab_change(event)
+  {
+    if(cache_cb) { cache_cb() }
   }
-);
+
+  $(document).ready(
+    function($) {
+    $('#ttt-body').show();
+
+    ce.form = $('#admin-tabs');
+
+    ce.tabs = {};
+    ce.form.find('button').each(
+      function() { ce.tabs[$(this).attr('value')] = $(this) }
+    );
+
+    ce.form.on('submit',handle_tab_change);
+  }
+  );
+
+})();
