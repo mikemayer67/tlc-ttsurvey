613b37b01567fa277fe9659c465ff112262afefb
Michael A Mayer <mikemayer67@vmwishes.com>
2025-05-28 01:46:30 -0400

    hints working

--- admin/js/editors.js
+++ admin/js/editors.js
@@ -34,8 +34,9 @@ export default function editors(ce)
   }
 
   const _sv_name              = _section_viewer.children('.name');
-  const _sv_name_value        = _sv_name.find('span.value');
-  const _sv_name_hide         = _sv_name.find('span.note');
+  const _sv_name_value        = _sv_name.find('div.text');
+  const _sv_show_name         = _section_viewer.children('.show-name');
+  const _sv_show_name_value   = _sv_show_name.find('div.text');
   const _sv_description       = _section_viewer.children('.description');
   const _sv_description_value = _sv_description.find('div.text');
   const _sv_feedback          = _section_viewer.children('.feedback');
@@ -44,9 +45,7 @@ export default function editors(ce)
   function show_section_viewer(section_id,section)
   {
     _sv_name_value.html(section.name);
-    if(section.show_name) { _sv_name_hide.hide() }
-    else                  { _sv_name_hide.show() }
-
+    _sv_show_name_value.html(section.show_name ? "YES" : "NO");
     _sv_description_value.html( section.description || '' );
     _sv_feedback_value.html( section.feedback || '' );
   }
@@ -56,38 +55,40 @@ export default function editors(ce)
   }
 
   const _qv_type              = _question_viewer.children('.type');
-  const _qv_type_value        = _qv_type.find('span.value');
-  const _qv_type_note         = _qv_type.find('span.note');
+  const _qv_type_value        = _qv_type.find('div.text');
   const _qv_wording           = _question_viewer.children('.wording');
-  const _qv_wording_value     = _qv_wording.filter('span.value');
+  const _qv_wording_value     = _qv_wording.find('div.text');
   const _qv_qualifier         = _question_viewer.children('.qualifier');
-  const _qv_qualifier_value   = _qv_qualifier.filter('span.value');
+  const _qv_qualifier_value   = _qv_qualifier.find('div.text');
   const _qv_description       = _question_viewer.children('.description');
-  const _qv_description_value = _qv_description.filter('span.value');
+  const _qv_description_value = _qv_description.find('div.text');
   const _qv_info              = _question_viewer.children('.info');
-  const _qv_info_label        = _qv_info.filter('span.label');
-  const _qv_info_value        = _qv_info.filter('span.value');
+  const _qv_info_label        = _qv_info.filter('label');
+  const _qv_info_value        = _qv_info.find('div.text');
+  const _qv_info_hint         = _qv_info.find('.hint > div');
+  const _qv_info_hint_info    = _qv_info_hint.filter('.info-block');
+  const _qv_info_hint_other   = _qv_info_hint.filter('.other-type');
   const _qv_options           = _question_viewer.children('.options');
   const _qv_primary           = _qv_options.filter('.primary');
   const _qv_secondary         = _qv_options.filter('.secondary');
   const _qv_other             = _qv_options.filter('.other');
-  const _qv_primary_value     = _qv_primary.filter('.value');
-  const _qv_secondary_value   = _qv_secondary.filter('.value');
-  const _qv_other_value       = _qv_other.filter('.value');
+  const _qv_primary_value     = _qv_primary.find('div.text');
+  const _qv_secondary_value   = _qv_secondary.find('div.text');
+  const _qv_other_value       = _qv_other.find('div.text');
   
   function show_question_viewer(question_id,question,options)
   {
     _question_viewer.children().hide();
     _qv_type.show();
-    _qv_type_note.html('');
     _qv_info_label.html('Popup Hint:');
+    _qv_info_hint.hide();
     switch(question.type) {
       case 'INFO':
         _qv_type_value.html('Info Block');
-        _qv_type_note.html('(survey text, no response)');
         _qv_info.show();
         _qv_info_label.html('Info Text:');
         _qv_info_value.html(question.info || '');
+        _qv_info_hint_info.show();
         break;
       case 'BOOL':
         _qv_type_value.html('Simple Checkbox');
@@ -99,22 +100,23 @@ export default function editors(ce)
         _qv_description_value.html(question.description || '');
         _qv_info.show();
         _qv_info_value.html(question.info || '');
+        _qv_info_hint_other.show();
         break;
       case 'FREETEXT':
         _qv_type_value.html('Free text');
-        _qv_type_note.html('(reponse textbox)');
         _qv_wording.show();
         _qv_wording_value.html(question.wording || '');
         _qv_description.show();
         _qv_description_value.html(question.description || '');
         _qv_info.show();
         _qv_info_value.html(question.info || '');
+        _qv_info_hint_other.show();
         break;
       case 'OPTIONS':
         if(question.multiple) {
-          _qv_type_value.html('Multiple Select Options');
+          _qv_type_value.html('Multiple Selections');
         } else {
-          _qv_type_value.html('Single Select Option');
+          _qv_type_value.html('Single Select');
         }
         _qv_wording.show();
         _qv_wording_value.html(question.wording || '');
@@ -124,20 +126,21 @@ export default function editors(ce)
         _qv_description_value.html(question.description || '');
         _qv_info.show();
         _qv_info_value.html(question.info || '');
+        _qv_info_hint_other.show();
         _qv_options.show();
 
         const primary = question.options.filter(([id,secondary]) => !secondary);
         const secondary = question.options.filter(([id,secondary]) => secondary);
-        _qv_primary_value.html('');
+        _qv_primary_value.find('ul').remove();
         if(primary.length) {
-          const ul = $('<ul>').addClass('options').appendTo(_qv_primary_value);
+          const ul = $('<ul>').addClass('options').prependTo(_qv_primary_value);
           primary.map(([id,]) => options[id])
           .forEach((opt) => $('<li>').text(opt).appendTo(ul));
         }
 
-        _qv_secondary_value.html('');
+        _qv_secondary_value.find('ul').remove();
         if(secondary.length) {
-          const ul = $('<ul>').addClass('options').appendTo(_qv_secondary_value);
+          const ul = $('<ul>').addClass('options').prependTo(_qv_secondary_value);
           secondary.map(([id,]) => options[id])
           .forEach((opt) => $('<li>').text(opt).appendTo(ul));
         }
--- admin/survey_frame.php
+++ admin/survey_frame.php
@@ -2,70 +2,106 @@
 namespace tlc\tts;
 
 if(!defined('APP_DIR')) { error_log("Invalid entry attempt: ".__FILE__); die(); }
-echo <<<HTML
-<div id='editor-frame'>
-  <div class='content-header'>Section/Question Details</div>
 
-  <!--Section Editor-->
-  <div class='grid section editor'>
-    Section Editor
+function add_viewer_entry($section_prefix, $classes, $label_text, $hint_text) {
+  $entry_key = explode(' ',$classes)[0];
+  $cb_id = implode('-',[$section_prefix,$entry_key,'cb']);
+  echo "<label class='$classes label' for='$cb_id'>$label_text:</label>";
+  echo "<div class='$classes value'>";
+  echo "  <div class='text'></div>";
+  echo "  <input id='$cb_id' type='checkbox' class='hint-cb'></input>";
+  echo "  <div class='hint'>$hint_text</div>";
+  echo "</div>";
+}
+
+echo "<div id='editor-frame'>";
+echo "  <div class='content-header'>Section/Question Details</div>";
+echo "  <div class='hint-hint'>Click or hover on any of the entry labels for more info about that entry.</div>";
+
+
+echo "<!--Section Editor-->";
+echo "<div class='grid section editor'>";
+echo "  Section Editor";
+echo "</div>";
+
+
+echo "<!--Section Viewer-->";
+echo "<div class='grid section viewer'>";
+add_viewer_entry('sv','name','Name',
+  'Name used to identify this section.  If Show Name is YES, the section name will be included in the survey. Otherwise, it will only be used in the Admin Dashboard'
+);
+add_viewer_entry('sv','show-name','Show Name',
+  'Whether or not to show the section name in the survey'
+);
+add_viewer_entry('sv','description','Description',
+  '<b>This field is optional.</b>  If provided, it will be displayed in the survey before any of the questions or info text within this section.  The text can be stylized using markdown.'
+);
+add_viewer_entry('sv','feedback', 'Feedback',
+  '<b>This field is optional.</b>  It sepecifies the label used to introduce a free text entry at the end of the section. If this field is empty, no such free text entry will be included in the survey for this section.'
+);
+echo "  </div>";
+
+
+echo "  <!--Question Editor-->";
+echo "  <div class='grid question editor'>";
+echo "    Question Editor";
+echo "  </div>";
+
+
+echo "  <!--Question Viewer-->";
+echo "  <div class='grid question viewer'>";
+add_viewer_entry('qv','type','Type',
+<<<HINT
+Type of "question" entry in the survey. Possible values are:
+  <p><b>Info Block</b> - Not actually a question.  This is a block of information included in the survey.</p>
+  <p><b>Simple Checkbox</b> - For use with Yes/No questions.</p>
+  <p><b>Single Selection</b> - Participant can no more than one option, if any</p>
+  <p><b>Multiple Selections</b> - Participant can select as many options as apply</p>
+  <p><b>Free Text</b> - A text box is provided for participant to provide a response in their own words.</p>
+HINT
+);
+add_viewer_entry('qv','wording','Wording',
+  'The actual wording of the question on the survey'
+);
+add_viewer_entry('qv','description','Description',
+  "<b>This field is optional.</b>  If provided, this will appear in the survey to provide additional 
+  information (background, context, whatever...) about the question.  The text can be 
+  stylized using markdown.
+  <p><a href='https://www.markdownguide.org/basic-syntax' target='_blank'>Markdown Reference</a></p>"
+);
+add_viewer_entry('qv','primary options','Primary Options',
+  "For multiple choice questions, this is the primary list of options available to choose from.
+   There must be at least one option provided for the question to be valid.  Ideally, there
+   will be more than one option.  Otherwise, consider using a Simple Checkbox question."
+);
+add_viewer_entry('qv','secondary options','Secondary Options',
+  "<b>This field is optional.</b>  Secondary options are only shown to the participant if they have 
+  selected at least one of the primary options (assuming Javascript is enabled)."
+);
+add_viewer_entry('qv','other options','Other Options',
+  "<b>This field is optional.</b>  This used for multiple choice questions where the participant
+  should be allowed to fill in their own option.  If specified, this field provides the
+  prompty that will appear in the survey to introduce the other option.  If not specified,
+  the question will not include an input field for a participant provided option."
+);
+add_viewer_entry('qv','qualifier','Qualifier',
+  "<b>This field is optional.</b>  This is used when it would be useful to allow the participant
+  to provide additional information about their response.  If specified, this field provides
+  the prompt that will appear in the survey to introduce the input field used to 
+  qualify their response."
+);
+add_viewer_entry('qv','info','Info',
+<<<INFO
+  <div class='info-block'>The text of the information block.  
+  The text can be stylized using markdown.
+  <p><a href='https://www.markdownguide.org/basic-syntax' target='_blank'>Markdown Reference</a></p>
   </div>
-
-  <!--Section Viewer-->
-  <div class='grid section viewer'>
-    <span class='name label'>Name:</span>
-    <div class='name value'>
-      <span class='value'></span>
-      <span class='note'>(not shown in survey)</span>
-    </div>
-    <label class='description label' for='sv-description-cb'>Description:</label>
-    <div class='description value'>
-      <div class='text'></div>
-      <input id='sv-description-cb' type='checkbox' class='hint-cb'></input>
-      <div class='hint'>This field is optional.  If provided, it will be displayed in the survey before any of the questions or info text within this section.  The text can be stylized using markdown.</div>
-    </div>
-    <label class='feedback label' for='sv-feedback-cb'>Feedback:</label>
-    <div class='feedback value'>
-      <div class='text'></div>
-      <input id='sv-feedback-cb' type='checkbox' class='hint-cb'></input>
-      <div class='hint'>This field is optional.  It sepecifies the label used to introduce a free text entry at the end of the section. If this field is empty, no such free text entry will be included in the survey for this section.</div>
-   </div>
+  <div class='other-type'><b>This field is optional.</b>  If provided, it provides the text that
+  will appear in the popup hint associated with the question input fields.
   </div>
+INFO
+);
+echo "</div>";
 
-  <!--Question Editor-->
-  <div class='grid question editor'>
-    Question Editor
-  </div>
+echo "</div>";
 
-  <!--Question Viewer-->
-  <div class='grid question viewer'>
-    <span class='type label'>Type:</span>
-    <div class='type value'>
-      <span class='value'></span>
-      <span class='note'></span>
-    </div>
-
-    <span class='wording label'>Wording:</span>
-    <span class='wording value'></span>
-
-    <span class='description label'>Description:</span>
-    <span class='description value'></span>
-
-    <span class='primary options label'>Primary Options:</span>
-    <div class='primary options value'></div>
-
-    <span class='secondary options label'>Secondary Options:</span>
-    <div class='secondary options value'></div>
-
-    <span class='other options label'>Prompt for Other:</span>
-    <span class='other options value'></span>
-
-    <span class='qualifier label'>Qualifier:</span>
-    <span class='qualifier value'></span>
-
-    <span class='info label'>Info:</span>
-    <span class='info value'></span>
-  </div>
-</div>
-
-HTML;
