250dc60b95e7d219ee7232c77132b4e1e993e701
Michael A Mayer <mikemayer67@vmwishes.com>
2025-08-13 01:28:33 -0400

    factored preview into preview/render

--- survey/preview.php
+++ survey/preview.php
@@ -7,18 +7,15 @@ log_dev("-------------- Start of Preview --------------");
 
 validate_and_retain_nonce('preview');
 
-echo "<html><head><title>Survey Preview</title></head><body>";
-
 require_once(app_file('include/login.php'));
 $active_user = active_userid();
-echo "<h2>Active Userid: $active_user</h2>";
 log_dev("Active User: $active_user");
 
-$title = $_POST['title'] ?? '[No Name]';
-$content = json_decode($_POST['content'] ?? '',true);
-echo "<h1>Title</h1><pre>$title</pre>";
-echo "<h1>Content</h1><pre>".print_r($content,true)."</pre>";
+$page_title = 'Survey Preview';
 
-echo "</body></html>";
+$survey_title = $_POST['title'] ?? '[No Name]';
+$content = json_decode($_POST['content'] ?? '',true);
+
+require(app_file('survey/render.php'));
 
 die();
