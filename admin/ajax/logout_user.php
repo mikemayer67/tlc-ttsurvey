43e0193371a2f7836593ae13f8fbc050b10ce321
Michael A Mayer <mikemayer67@vmwishes.com>
2025-08-14 14:13:59 -0400

    admin login - add userid

---
+++ admin/ajax/logout_user.php
@@ -0,0 +1,16 @@
+<?php
+namespace tlc\tts;
+
+if(!defined('APP_DIR')) { error_log("Invalid entry attempt: ".__FILE__); die(); }
+
+require_once(app_file('include/logger.php'));
+require_once(app_file('include/login.php'));
+
+validate_ajax_nonce('admin-navbar');
+
+log_info("Logging out Active User");
+
+logout_active_user();
+
+echo json_encode(['success'=>true]);
+die();
